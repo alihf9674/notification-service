842fcee1ea2d8b1250b318058712225fdffdf095
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-06-24 22:49:26 +0330

    Add persian language validation

---
+++ resources/lang/fa/validation.php
@@ -0,0 +1,10 @@
+<?php
+return[
+    'integer' => ':attribute باید به صورت عددی باشد',
+    'exists' => ':attribute باید به صورت صحیح وارد شود',
+
+    'attributes' => [
+        'email_type' => 'نوع ایمیل',
+        'user' => 'کاربر'
+    ]
+];
